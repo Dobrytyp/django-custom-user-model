a4274b18404ce7879e1dccb25b7597a8279da4a4
dobrytyp@gmail.com <Marcelino2019>
2020-10-23 19:44:40 +0200

    Initial commit

---
+++ users/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+# W ramach tego modelu będą pola: username, email, password (chociaż tego pola akurat nie trzeba definiować),
+# is_superuser, is_active, is_verified, is_admin, date_joined (pole datetime) oraz date_last_login (pole datatime).
